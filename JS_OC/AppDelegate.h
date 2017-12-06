fee37c856cfa16e0d1b14bb6b3692b66962b30ed
zhangyoubo <13818956024@163.com>
2017-12-06 15:26:23 +0800

    Initial Commit

---
+++ JS_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JS_OC
+//
+//  Created by  on 2017/12/6.
+//  Copyright © 2017年 张友波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JS_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JS_OC
+//
+//  Created by  on 2017/12/6.
+//  Copyright © 2017年 张友波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
